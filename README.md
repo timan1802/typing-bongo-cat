7bb92eaed80f886e5e9021ceb8026a602dd7b66b
NDH <timan@naver.com>
2025-07-15 14:55:03 +0900

    .

---
+++ README.md
@@ -0,0 +1,20 @@
+Typing Bongo Cat  IntelliJ plugin
+======================
+
+Installation
+--------------
+
+[Install from Jetbrains plugin](https://plugins.jetbrains.com/) 
+
+
+This plugin was created to add a little bit of joy to your coding experience. 
+When you type on your keyboard, a Bongo Cat appears at your caret position, showing a lovely animation of it tapping its keyboard right along with you.
+
+
+![Demo](/images/demo.gif)
+--------------
+
+This plug-in was inspired by the plug-in below.
+
+https://plugins.jetbrains.com/plugin/22672-bongocat
+https://plugins.jetbrains.com/plugin/8251-power-mode-ii
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "2.1.0"
     id("org.jetbrains.intellij.platform") version "2.5.0"
 }
 
@@ -22,15 +21,10 @@ java {
     }
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
 dependencies {
     intellijPlatform {
         create("IC", "2025.1")
         testFramework(TestFrameworkType.Platform)
-
-        // Add necessary plugin dependencies for compilation here, example:
-        // bundledPlugin("com.intellij.java")
     }
 }
 
